986f836f11d28bb1bd5ee4f5c4893fe058b2840f
MasterProgrammer79 <julian.varciag@gmail.com>
2014-11-08 18:18:01 -0600

    Updated master file

--- LinkedU/LinkedU/LinkedU.Master.designer.cs
+++ LinkedU/LinkedU/LinkedU.Master.designer.cs
@@ -31,6 +31,24 @@ namespace LinkedU {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// LogoHome control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink LogoHome;
+        
+        /// <summary>
+        /// MenuHome control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink MenuHome;
+        
+        /// <summary>
         /// Login control.
         /// </summary>
         /// <remarks>
