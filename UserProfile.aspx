cf6689d0c0a0586a22ef78d6fe5a22cf5628d9d8
ektapaliwal <empaliw@ilstu.edu>
2014-11-16 22:50:10 -0600

    profile and view profile added

---
+++ UserProfile.aspx
@@ -0,0 +1,182 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/LinkedU.Master" AutoEventWireup="true" CodeBehind="UserProfile.aspx.cs" Inherits="LinkedU.UserProfile" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    
+        <div class="container">
+               <div class="site full">
+            <h1 style="text-align:center">Edit Your Profile Linked<span style="color:#4472c4">U</span> Account</h1>
+        </div>
+            <div class="site site sign-up-panel-2">
+                <div class="sign-up-form">
+                    <asp:Panel ID="photouploadPanel" runat="server">
+                    <asp:Image ID="ImagePreview" runat="server" ImageUrl="~/images/noprofileimg.png" Height="164px" Width="187px" />
+                    <asp:Label ID="userName" runat="server" Font-Bold="true" Font-Size="Large" ></asp:Label>
+                    <asp:FileUpload ID="PhotoUpload" runat="server" CssClass="btn btn-primary"  />
+                     <asp:Label ID="choosePhotolbl" runat="server"></asp:Label><br />
+                    <asp:Button ID="choosePhoto" runat="server" Text="Save" OnClick="choosePhoto_Click" CssClass="btn btn-primary" />
+                    
+                    </asp:Panel>
+
+                </div>
+
+            </div>
+            <div class="site sign-up-panel-2">
+                <div class="sign-up-form">
+                    <h2 style="text-align:center">School Information</h2>
+                    <hr />
+            <asp:Panel ID="profilePanel1" runat="server">
+            <table>  
+                <tr><td>
+            Summary: </td>
+               <td> <asp:TextBox ID="summaryTxt" runat="server" TextMode="MultiLine" Height="61px" Width="163px" CssClass="form-control"></asp:TextBox>
+           </td></tr>
+                        <tr>
+                            <td>
+                                <asp:Label ID="Label1" runat="server" Text="School Name"></asp:Label>
+                            </td>
+                            <td>
+                    <asp:TextBox ID="schoolName" runat="server" CssClass="form-control"></asp:TextBox>
+                                </td>
+                            </tr>
+                        <tr>
+                            <td>
+                                <asp:Label ID="Label2" runat="server" Text="Field of Study"></asp:Label>
+                            </td>
+                            <td>
+                     <asp:TextBox ID="fieldOfStudy" runat="server" CssClass="form-control"></asp:TextBox>
+                            </td></tr>
+                        <tr><td>
+                                <asp:Label ID="Label3" runat="server" Text="Major"></asp:Label>
+                            </td>
+                            <td>
+                     <asp:TextBox ID="major" runat="server" CssClass="form-control"></asp:TextBox>
+                           </td> </tr>
+                        <tr>
+                            <td>
+                                <asp:Label ID="Label4" runat="server" Text="Minor"></asp:Label>
+                            </td>
+                            <td>
+                    <asp:TextBox ID="minor" runat="server" CssClass="form-control"></asp:TextBox>
+                          </td>  </tr>
+                    <tr>
+                        <asp:Label ID="saveStatus" runat="server"></asp:Label>
+
+                    </tr>
+                        </table>
+                <asp:Button ID="saveButtonStep1" runat="server" Text="Save" OnClick="saveButtonStep1_Click" CssClass="btn btn-primary" /> 
+                <asp:Button ID="NextButton1" runat="server" Text="Next Step" OnClick="NextButton1_Click" CssClass="btn btn-primary" />
+                  
+            </asp:Panel>
+            </div>
+        </div>
+             </div>
+                <asp:Panel ID="OtherInfoPanel" runat="server" style="display:none">
+            <div class="site sign-up-panel-2">
+                <div class="sign-up-form">
+                    <h2 style="text-align:center">Other Information</h2>
+                    <hr />
+                    <table>  
+                <tr><td>
+            Cumulative GPA </td>
+                <td><asp:TextBox ID="gpaTxt" runat="server" TextMode="Number" CssClass="form-control"></asp:TextBox>
+           </td></tr>
+                        <tr>
+                            <td>
+                                ACT Score
+                            </td>
+                            <td>
+                    <asp:TextBox ID="actScoreTxt" runat="server" TextMode="Number" CssClass="form-control"></asp:TextBox>
+                                </td>
+                            </tr>
+                        <tr>
+                            <td>
+                                SAT Score
+                            </td>
+                            <td>
+                     <asp:TextBox ID="satScoreTxt" runat="server" TextMode="Number" CssClass="form-control"></asp:TextBox>
+                            </td></tr>
+                        <tr><td>
+                                PSAT Score:
+                            </td>
+                            <td>
+                     <asp:TextBox ID="psatScoreTxt" runat="server" TextMode="Number" CssClass="form-control"></asp:TextBox>
+                           </td> </tr>
+                        <tr>
+                            <td>
+                                Add Your Skills
+                            </td>
+                            <td>
+                    <asp:TextBox ID="skillsTxt" runat="server"  CssClass="form-control"></asp:TextBox>
+                          </td>  </tr>
+                    <tr>
+                        <asp:Label ID="savestatus2" runat="server"></asp:Label>
+
+                    </tr>
+                        </table>
+                <asp:Button ID="saveOtherInfobtn" runat="server" Text="Save" CssClass="btn btn-primary" OnClick="saveOtherInfobtn_Click" /> 
+                <asp:Button ID="NextButton2" runat="server" Text="Next Step" CssClass="btn btn-primary" OnClick="NextButton2_Click"  />
+                     </div>
+        </div>
+    </asp:Panel>
+
+                        <asp:Panel ID="educationInformation" runat="server" style="display:none">
+            <div class="site sign-up-panel-2">
+                <div class="sign-up-form">
+                    <h2 style="text-align:center">Education Information</h2>
+                    <hr />
+                    <table>  
+                        <tr>
+                            <td>
+                                Universities of Choice
+                            </td>
+                            <td>
+                    <asp:TextBox ID="TextBox2" runat="server" CssClass="form-control"></asp:TextBox>
+                                </td>
+                            
+                            </tr>
+                        
+                           <%-- <tr> <td runat="server">
+                                <asp:Panel ID="pnlTextBoxes" runat="server">
+                                </asp:Panel>
+                                <%--<asp:PlaceHolder ID="phTextBoxes" runat="server">
+                                </asp:PlaceHolder>  </td>
+                           
+                           
+                        </tr>
+                        <tr><td><asp:Button ID="AddTextBox" runat="server" Text="Add" CssClass="btn btn-primary" OnClick="AddTextBox_Click"  /></td></tr>  --%>
+                            <tr>
+                            <td>
+                                Majors of Choice
+                            </td>
+                            <td>
+                     <asp:TextBox ID="TextBox3" runat="server"  CssClass="form-control"></asp:TextBox>
+                            </td></tr>
+                        <tr><td>
+                                Essay of Choice
+                            </td>
+                            <td>
+                                <asp:FileUpload ID="EsaayOfChoiceUpload" runat="server" />
+                           </td>
+                            <td><asp:Label ID="lblEsaayMessage" runat="server"></asp:Label></td>
+
+                        </tr>
+                        <tr>
+                            <td>
+                                Mix Tape of Activities
+                            </td>
+                            <td>
+                             <asp:FileUpload ID="uploadVideo" runat="server" />
+                          </td>  </tr>
+                    <tr>
+                        <asp:Label ID="Label5" runat="server"></asp:Label>
+
+                    </tr>
+                        </table>
+                <asp:Button ID="Save3" runat="server" Text="Save" CssClass="btn btn-primary" OnClick="Save3_Click" /> 
+                <asp:Button ID="Finish" runat="server" Text="Next Step" CssClass="btn btn-primary" OnClick="NextButton2_Click"  />
+                     </div>
+        </div>
+    </asp:Panel>
+           
+</asp:Content>
---
+++ UserProfile.aspx.cs
@@ -0,0 +1,336 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Web.Configuration;
+using System.IO;
+using System.Data;
+
+namespace LinkedU
+{
+    public partial class UserProfile : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                if (HttpContext.Current.Session["userID"] != null)
+                {
+                    userName.Text = getFirstName(); 
+                     string connStr = ConfigurationManager.ConnectionStrings["ProjectDB"].ConnectionString;
+                    SqlConnection dbConnection = new SqlConnection(connStr);
+           
+                int uid = Convert.ToInt32(HttpContext.Current.Session["userID"]);
+                dbConnection.Open();
+                string getImagePath = "select ImageFilePath from userImageTable where userID=@userID";
+                SqlCommand sqlCommandgetPath = new SqlCommand(getImagePath, dbConnection);
+                sqlCommandgetPath.Parameters.Add(new SqlParameter("@userID", uid));
+                SqlDataReader reader = sqlCommandgetPath.ExecuteReader();
+                if (reader.Read())
+                {
+                   string filePath = "~/"+reader.GetString(0);
+                   ImagePreview.ImageUrl = filePath;
+
+                }
+                   
+                }
+            }
+        }
+
+        protected void saveButtonStep1_Click(object sender, EventArgs e)
+        {
+            string connStr = ConfigurationManager.ConnectionStrings["ProjectDB"].ConnectionString;
+            SqlConnection dbConnection = new SqlConnection(connStr);
+            int uidDB=0;
+            int uid = Convert.ToInt32(HttpContext.Current.Session["userID"]);
+                dbConnection.Open();
+                string checkUserID = "select userID from studentProfessional where userID=@userID";
+                SqlCommand sqlCommandCheck = new SqlCommand(checkUserID, dbConnection);
+                sqlCommandCheck.Parameters.Add(new SqlParameter("@userID", uid));
+                SqlDataReader reader = sqlCommandCheck.ExecuteReader();
+                if (reader.Read())
+                {
+                    uidDB = reader.GetInt32(0);
+                    reader.Close();
+                    string updateuserInfo = "UPDATE studentProfessional set summary=@summary, pschoolName=@schoollName,fieldOfStudy=@fieldOfStudy, major=@major,minor=@minor where userID=" +uid;
+                    SqlCommand sqlCommand = new SqlCommand(updateuserInfo, dbConnection);
+                    sqlCommand.Parameters.AddWithValue("@summary", summaryTxt.Text);
+                    sqlCommand.Parameters.AddWithValue("@schoollName", schoolName.Text);
+                    sqlCommand.Parameters.AddWithValue("@fieldOfStudy", fieldOfStudy.Text);
+                    sqlCommand.Parameters.AddWithValue("@major", major.Text);
+                    sqlCommand.Parameters.AddWithValue("@minor", minor.Text);
+                    sqlCommand.ExecuteNonQuery();
+                    saveStatus.Text = "Your profile has been updated successfully";
+                }
+                else{
+                    reader.Close();
+                    string userInfo = "INSERT INTO studentProfessional (userID, major, summary, pschoolName, fieldOfStudy, minor) VALUES(@userID, @summary, @schoollName,@fieldOfStudy,@major,@minor)";
+                    SqlCommand sqlCommand = new SqlCommand(userInfo, dbConnection);
+                    sqlCommand.Parameters.AddWithValue("@userID", uid);
+                    sqlCommand.Parameters.AddWithValue("@summary", summaryTxt.Text);
+                    sqlCommand.Parameters.AddWithValue("@schoollName", schoolName.Text);
+                    sqlCommand.Parameters.AddWithValue("@fieldOfStudy", fieldOfStudy.Text);
+                    sqlCommand.Parameters.AddWithValue("@major", major.Text);
+                    sqlCommand.Parameters.AddWithValue("@minor", minor.Text);
+                    sqlCommand.ExecuteNonQuery();
+                    saveStatus.Text = "Your profile has been saved successfully";
+                    saveStatus.Font.Bold = true;
+                    
+                }
+        }
+
+ protected string getFirstName()
+        {
+            SqlConnection dbConnection = new SqlConnection(WebConfigurationManager.ConnectionStrings["ProjectDB"].ConnectionString);
+            int uid = Convert.ToInt32(HttpContext.Current.Session["userID"]);
+            string firstname = "";
+            try
+            {
+                dbConnection.Open();
+                string query = "select userType from userDetails where userID = @uid";
+                try
+                {
+                    SqlCommand getType = new SqlCommand(query, dbConnection);
+                    getType.Parameters.Add(new SqlParameter("@uid", uid));
+                    SqlDataReader reader = getType.ExecuteReader();
+                    string nameQuery = "";
+                    reader.Read();
+                    if (reader.GetString(0).Equals("student"))
+                    {
+                        nameQuery = "select firstName, lastName from studentPersonal where userID = @uid";
+                    }
+                    else
+                    {
+                        nameQuery = "select recruiterName, recruiterLastName from recruiter where userID = @uid";
+                    }
+                    reader.Close();
+                    try
+                    {
+                        SqlCommand getName = new SqlCommand(nameQuery, dbConnection);
+                        getName.Parameters.Add(new SqlParameter("@uid", uid));
+                        SqlDataReader nameReader = getName.ExecuteReader();
+                        nameReader.Read();
+                        firstname = nameReader.GetString(0).ToUpper()+ " " + nameReader.GetString(1).ToUpper();
+                        nameReader.Close();
+                    }
+                    catch (SqlException ez)
+                    {
+                        Response.Write("<p>Error Code " + ez.Number + ": " + ez.Message + "</p>");
+                    }
+                }
+                catch (SqlException ey)
+                {
+                    Response.Write("<p>Error Code " + ey.Number + ": " + ey.Message + "</p>");
+                }
+                dbConnection.Close();
+            }
+            catch (SqlException ex)
+            {
+                Response.Write("<p>Error Code " + ex.Number + ": " + ex.Message + "</p>");
+            }
+            return firstname;
+        }
+
+                 protected void NextButton1_Click(object sender, EventArgs e)
+                 {
+                     OtherInfoPanel.Attributes.Remove("style");
+                     OtherInfoPanel.Attributes.Add("style", "display:block");
+                 }
+
+                 protected void NextButton2_Click(object sender, EventArgs e)
+                 {
+                     educationInformation.Attributes.Remove("style");
+                     educationInformation.Attributes.Add("style", "display:block");
+                 }
+
+                 protected void saveOtherInfobtn_Click(object sender, EventArgs e)
+                 {
+                     string connStr = ConfigurationManager.ConnectionStrings["ProjectDB"].ConnectionString;
+                     SqlConnection dbConnection = new SqlConnection(connStr);
+                     int uidDB = 0;
+                     int uid = Convert.ToInt32(HttpContext.Current.Session["userID"]);
+                     dbConnection.Open();
+                     string checkUserID = "select userID from studentScore where userID=@userID";
+                     SqlCommand sqlCommandCheck = new SqlCommand(checkUserID, dbConnection);
+                     sqlCommandCheck.Parameters.Add(new SqlParameter("@userID", uid));
+                     SqlDataReader reader = sqlCommandCheck.ExecuteReader();
+                     if (reader.Read())
+                     {
+                         uidDB = reader.GetInt32(0);
+                         reader.Close();
+                         string updateuserInfo = "UPDATE studentScore set ACT=@act, SAT=@sat, PSAT=@psat, GPA=@gpa, skill=@skill where userID=" + uid;
+                         //string updateuserInfoScore = "UPDATE studentScore set ACT=@act, SAT=@sat, PSAT=@psat where userID=" + uid;
+                         SqlCommand sqlCommand = new SqlCommand(updateuserInfo, dbConnection);
+                         sqlCommand.Parameters.AddWithValue("@act", actScoreTxt.Text);
+                         sqlCommand.Parameters.AddWithValue("@sat", satScoreTxt.Text);
+                         sqlCommand.Parameters.AddWithValue("@psat", psatScoreTxt.Text);
+                         sqlCommand.Parameters.AddWithValue("@gpa", gpaTxt.Text);
+                         sqlCommand.Parameters.AddWithValue("@skill", skillsTxt.Text);
+                         sqlCommand.ExecuteNonQuery();
+                         savestatus2.Text = "Your profile has been updated successfully";
+                         savestatus2.Font.Bold = true;
+                      
+                       }
+                       else {
+                           reader.Close();
+                           string userScoreInfo = "INSERT INTO studentScore (userID,ACT, SAT, PSAT, GPA, skill) VALUES(@userID, @act, @sat, @psat, @gpa, @skill)";
+                           SqlCommand sqlCommandScore = new SqlCommand(userScoreInfo, dbConnection);
+                           sqlCommandScore.Parameters.AddWithValue("@userID", uid);
+                           sqlCommandScore.Parameters.AddWithValue("@act", actScoreTxt.Text);
+                           sqlCommandScore.Parameters.AddWithValue("@sat", satScoreTxt.Text);
+                           sqlCommandScore.Parameters.AddWithValue("@psat", psatScoreTxt.Text);
+                         sqlCommandScore.Parameters.AddWithValue("@gpa", gpaTxt.Text);
+                         sqlCommandScore.Parameters.AddWithValue("@skill", skillsTxt.Text);
+                           sqlCommandScore.ExecuteNonQuery();
+                           savestatus2.Text = "Your profile has been saved successfully";
+                           savestatus2.Font.Bold = true;
+                       }
+                         
+                     }
+                     
+                     
+                 
+
+                 protected void choosePhoto_Click(object sender, EventArgs e)
+                 {
+                     Session["ImageBytes"] = PhotoUpload.FileBytes;
+                     ImagePreview.ImageUrl = "~/ImageHandler.ashx";
+                     if (PhotoUpload.HasFile)
+                     {
+                         // Get the file extension
+                         string fileExtension = System.IO.Path.GetExtension(PhotoUpload.FileName);
+
+                         if (fileExtension.ToLower() != ".png" && fileExtension.ToLower() != ".jpg" && fileExtension.ToLower() != ".gif")
+                         {
+                             choosePhotolbl.ForeColor = System.Drawing.Color.Red;
+                             choosePhotolbl.Text = "Only files with .png and .jpg extension are allowed";
+                         }
+                         else
+                         {
+                             // Get the file size
+                             int fileSize = PhotoUpload.PostedFile.ContentLength;
+                             // If file size is greater than 2 MB
+                             if (fileSize > 2097152)
+                             {
+                                 choosePhotolbl.ForeColor = System.Drawing.Color.Red;
+                                 choosePhotolbl.Text = "File size cannot be greater than 2 MB";
+                             }
+                             else
+                             {
+                                 // Upload the file
+                                 PhotoUpload.SaveAs(Server.MapPath("~/userImages/" + getFirstName()+PhotoUpload.FileName));
+                                 choosePhotolbl.ForeColor = System.Drawing.Color.Green;
+                                 choosePhotolbl.Text = "File uploaded successfully";
+                                 string connStr = WebConfigurationManager.ConnectionStrings["ProjectDB"].ConnectionString;
+                                 SqlConnection dbConnection = new SqlConnection(connStr);
+                                 dbConnection.Open();
+                                 int uid = Convert.ToInt32(HttpContext.Current.Session["userID"]);
+                                 string path = "userImages/" + getFirstName() + PhotoUpload.FileName;
+                                 string strQuery = "insert into userImageTable (userID, ImageFileName, ImageFilePath) values (@userID, @FileName, @FilePath)";
+                                 SqlCommand cmd = new SqlCommand(strQuery, dbConnection);
+                                 cmd.Parameters.AddWithValue("@userID", uid);
+                                 cmd.Parameters.AddWithValue("@FileName", getFirstName() + PhotoUpload.FileName);
+                                 cmd.Parameters.AddWithValue("@FilePath", path);
+                                 cmd.ExecuteNonQuery();
+
+
+                             }
+                         }
+                     }
+                     else
+                     {
+                         choosePhotolbl.ForeColor = System.Drawing.Color.Red;
+                         choosePhotolbl.Text = "Please select a file";
+                     } 
+                     
+                 }
+
+                 //TextBox tb;
+                 //Label labl;
+                 //static int i = 0;
+                 //protected void AddTextBox_Click(object sender, EventArgs e)
+                 //{ 
+                     
+                 //    for (int j = 0; j <= i; j++)
+                 //    {
+                 //        tb = new TextBox();
+                 //        tb.ID = j.ToString();
+                 //        tb.CssClass = "form-control";
+                 //        string textLabel = "<br /> University Name";
+                 //        labl = new Label();
+                 //        labl.Text = textLabel;
+                 //        pnlTextBoxes.Controls.Add(labl);
+
+                 //        pnlTextBoxes.Controls.Add(tb);
+                         
+                 //    }
+                 //    i++;  
+                 //foreach (Control ctr in pnlTextBoxes.Controls)
+                 //{
+                 //    if (ctr is TextBox)
+                 //    {
+                 //        string listofUniv = ((TextBox)ctr).Text + ",";
+                 //        test.Text = listofUniv;
+                 //    }
+                 //}
+                     
+                 //}
+
+                 protected void Save3_Click(object sender, EventArgs e)
+                 {
+                     if (EsaayOfChoiceUpload.HasFile)
+                     {
+                         // Get the file extension
+                         string fileExtension = System.IO.Path.GetExtension(EsaayOfChoiceUpload.FileName);
+
+                         if (fileExtension.ToLower() != ".doc" && fileExtension.ToLower() != ".docx" && fileExtension.ToUpper() != ".pdf")
+                         {
+                             lblEsaayMessage.ForeColor = System.Drawing.Color.Red;
+                             lblEsaayMessage.Text = "Only files with .doc, .pdf and .docx extension are allowed";
+                         }
+                         else
+                         {
+                             // Get the file size
+                             int fileSize = EsaayOfChoiceUpload.PostedFile.ContentLength;
+                             // If file size is greater than 2 MB
+                             if (fileSize > 2097152)
+                             {
+                                 lblEsaayMessage.ForeColor = System.Drawing.Color.Red;
+                                 lblEsaayMessage.Text = "File size cannot be greater than 2 MB";
+                             }
+                             else
+                             {
+                                 // Upload the file
+                                 EsaayOfChoiceUpload.SaveAs(Server.MapPath("~/userEssay/" + getFirstName() + EsaayOfChoiceUpload.FileName));
+                                 lblEsaayMessage.ForeColor = System.Drawing.Color.Green;
+                                 lblEsaayMessage.Text = "File uploaded successfully";
+                                 string connStr = WebConfigurationManager.ConnectionStrings["ProjectDB"].ConnectionString;
+                                 SqlConnection dbConnection = new SqlConnection(connStr);
+                                 dbConnection.Open();
+                                 int uid = Convert.ToInt32(HttpContext.Current.Session["userID"]);
+                                 string path = "userEssay/" + getFirstName() + EsaayOfChoiceUpload.FileName;
+                                 string strQuery = "insert into userEssayTable (userID, EssayFileName, EssayFilePath) values (@userID, @FileName, @FilePath)";
+                                 SqlCommand cmd = new SqlCommand(strQuery, dbConnection);
+                                 cmd.Parameters.AddWithValue("@userID", uid);
+                                 cmd.Parameters.AddWithValue("@FileName", getFirstName() + EsaayOfChoiceUpload.FileName);
+                                 cmd.Parameters.AddWithValue("@FilePath", path);
+                                 cmd.ExecuteNonQuery();
+                             }
+                         }
+                     }
+                     else
+                     {
+                         lblEsaayMessage.ForeColor = System.Drawing.Color.Red;
+                         lblEsaayMessage.Text = "Please select a file";
+                     } 
+                     
+
+                 }
+
+                 
+    }
+}
