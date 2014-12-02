6efda9ed82104c284db004bb5e70c0a31d9eba31
mohitrathore <mrathor@ilstu.edu>
2014-12-02 14:42:41 -0600

    updated default.aspx

--- Default.aspx
+++ Default.aspx
@@ -4,9 +4,31 @@
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
     <div class="container">
+        <asp:Panel ID="PanelWelcome" runat="server">
         <div class="full site">
             <h1 style="text-align:center">Welcome to LinkedU!</h1>
-            <p>Please browser around our website. Check out the various Student and Recruiter profiles. Sign up for an account, or login if you are an existing user.</p>
+            <p>Please browse around our website. Check out the various Student and Recruiter profiles. Sign up for an account, or login if you are an existing user.</p>
         </div>
+            </asp:Panel>
+       
+       <div class="container" style="padding-left:250px" >
+        <div class="site login" style="margin-left:670px; width:250px">
+      
+        <asp:Panel ID="RequestForInfo" runat="server"  style="display:none">
+                    <div class="alert alert-danger" role="alert" style="height:100px">
+                        <asp:Label ID="RequestLabel" runat="server" Text="Label"></asp:Label>
+                          <asp:Button ID="AcceptButton" runat="server" Text="Accept" CssClass="btn btn-primary"  style="float:left" OnClick="accept_Click" />
+                          <asp:Button ID="DeleteButton" runat="server" Text="Delete" CssClass="btn btn-primary"  style="float:right" OnClick="delete_Click" />
+                    </div>
+                </asp:Panel>
+             
+            <asp:Panel ID="Descritionofrequest" runat="server"  style="display:none">
+                    <div class="alert alert-danger" role="alert">
+                        <asp:Label ID="Description" runat="server" Text="Label"></asp:Label><br />
+                        <asp:FileUpload ID="FileUpload1" runat="server" />
+                    </div>
+                </asp:Panel>
+           
+            </div></div>
     </div>
 </asp:Content>
--- Default.aspx.cs
+++ Default.aspx.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -11,6 +13,183 @@ namespace LinkedU
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (HttpContext.Current.Session["userID"] != null)
+            {
+                PanelWelcome.Attributes.Add("style", "display:none");
+                string requestInfo = getRequest();
+                if (requestInfo != "")
+                {
+                    RequestForInfo.Attributes.Remove("style");
+                    RequestLabel.Text = requestInfo;
+                }
+            }
+        }
+        protected void accept_Click(object sender, EventArgs e)
+        {
+            RequestForInfo.Attributes.Remove("style");
+            Descritionofrequest.Attributes.Remove("style");
+
+            SqlConnection dbConnection = new SqlConnection(WebConfigurationManager.ConnectionStrings["ProjectDB"].ConnectionString);
+            int uid = Convert.ToInt32(HttpContext.Current.Session["userID"]);
+            string request = "";
+
+            try
+            {
+                dbConnection.Open();
+                string query = "select * from moreinformation where studentID = @uid";
+                try
+                {
+                    SqlCommand getType = new SqlCommand(query, dbConnection);
+                    getType.Parameters.Add(new SqlParameter("@uid", uid));
+                    SqlDataReader reader = getType.ExecuteReader();
+                    string fromUser = "";
+
+                    if (reader.Read())
+                    {
+
+                        if (reader.GetString(1).Equals(null))
+                        {
+                            request = "";
+                            //notificationNew = "select notification from notification where studentID = @uid";
+
+                        }
+                        else
+                        {
+                            Description.Text = reader.GetString(1);
+                            reader.Close();
+
+                        }
+
+                        reader.Close();
+
+                    }
+
+                    else
+                    {
+
+                        reader.Close();
+
+                    }
+                }
+                catch (SqlException ey)
+                {
+                    Response.Write("<p>Error Code " + ey.Number + ": " + ey.Message + "</p>");
+                }
+                dbConnection.Close();
+            }
+            catch (SqlException ex)
+            {
+                Response.Write("<p>Error Code " + ex.Number + ": " + ex.Message + "</p>");
+            }
+
+
+
+        }
+        protected void delete_Click(object sender, EventArgs e)
+        {
+            RequestForInfo.Attributes.Add("style", "display:none");
+
+            SqlConnection dbConnection = new SqlConnection(WebConfigurationManager.ConnectionStrings["ProjectDB"].ConnectionString);
+            int uid = Convert.ToInt32(HttpContext.Current.Session["userID"]);
+
+
+            try
+            {
+                dbConnection.Open();
+                string query = "delete from moreinformation where studentID = @uid";
+                try
+                {
+                    SqlCommand getType = new SqlCommand(query, dbConnection);
+                    getType.Parameters.Add(new SqlParameter("@uid", uid));
+                    getType.ExecuteNonQuery();
+
+                }
+                catch (SqlException ey)
+                {
+                    Response.Write("<p>Error Code " + ey.Number + ": " + ey.Message + "</p>");
+                }
+                dbConnection.Close();
+            }
+            catch (SqlException ex)
+            {
+                Response.Write("<p>Error Code " + ex.Number + ": " + ex.Message + "</p>");
+            }
+
+
+        }
+        protected string getRequest()
+        {
+            SqlConnection dbConnection = new SqlConnection(WebConfigurationManager.ConnectionStrings["ProjectDB"].ConnectionString);
+            int uid = Convert.ToInt32(HttpContext.Current.Session["userID"]);
+            string request = "";
+
+            try
+            {
+                dbConnection.Open();
+                string query = "select * from moreinformation where studentID = @uid";
+                try
+                {
+                    SqlCommand getType = new SqlCommand(query, dbConnection);
+                    getType.Parameters.Add(new SqlParameter("@uid", uid));
+                    SqlDataReader reader = getType.ExecuteReader();
+                    string fromUser = "";
+
+                    if (reader.Read())
+                    {
+
+                        if (reader.GetString(1).Equals(null))
+                        {
+                            request = "";
+                            //notificationNew = "select notification from notification where studentID = @uid";
+
+                        }
+                        else
+                        {
+                            fromUser = reader.GetString(3);
+                            request = fromUser + " want some more infromation from you";
+                            reader.Close();
+
+                        }
+
+                        reader.Close();
+                        //try
+                        //{
+                        //    SqlCommand getName = new SqlCommand(notificationOld, dbConnection);
+                        //    getName.Parameters.Add(new SqlParameter("@uid", uid));
+                        //    SqlDataReader nameReader = getName.ExecuteReader();
+                        //    while (nameReader.Read())
+                        //    {
+
+                        //        notifications1.Add(nameReader.GetString(0));
+                        //    }
+                        //    nameReader.Close();
+                        //}
+
+                        //catch (SqlException ez)
+                        //{
+                        //    Response.Write("<p>Error Code " + ez.Number + ": " + ez.Message + "</p>");
+                        //}
+                    }
+
+                    else
+                    {
+
+                        reader.Close();
+                        //notifications1.Add("You have no notification");
+
+                    }
+                }
+                catch (SqlException ey)
+                {
+                    Response.Write("<p>Error Code " + ey.Number + ": " + ey.Message + "</p>");
+                }
+                dbConnection.Close();
+            }
+            catch (SqlException ex)
+            {
+                Response.Write("<p>Error Code " + ex.Number + ": " + ex.Message + "</p>");
+            }
+            return request;
 
         }
     }
